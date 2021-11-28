a5d1887947ab6711de8b39f6fceeff6938413f72
leigp <907090585@qq.com>
2021-11-28 15:26:01 +0800

    完成首页CountType组件样式书写

---
+++ src/components/CountType.vue
@@ -0,0 +1,26 @@
+<template>
+<div class="type" v-bind="$attrs">
+  <div>支出</div>
+  <div>收入</div>
+</div>
+</template>
+
+<script lang="ts">
+export default {
+  name: "CountType"
+}
+</script>
+
+<style lang="scss" scoped>
+.type{
+  display: flex;
+  justify-content: center;
+  >div{
+    flex-grow: 1;
+    text-align: center;
+    font-size: 24px;
+    padding: 16px 0;
+    background-color: #c4c4c4;
+  }
+}
+</style>
--- src/components/Record.vue
+++ src/components/Record.vue
@@ -1,11 +1,13 @@
 <template>
   <div class="container">
+    <CountType/>
    <KeyBord/>
   </div>
 </template>
 
 <script lang="ts" setup>
 import KeyBord from "./KeyBord.vue";
+import CountType from "./CountType.vue";
 </script>
 
 <style lang="scss" scoped>
