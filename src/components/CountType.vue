43cfa81ee413c97292e31c4298bb83af071dfa0f
leigp <907090585@qq.com>
2021-11-30 20:29:39 +0800

    countType.vue 组件动画制作

--- src/components/CountType.vue
+++ src/components/CountType.vue
@@ -1,16 +1,25 @@
 <template>
-  <div class="type" @click="toggle">
+  <div class="type" @click="toggle" ref="root" >
     <div >支出</div>
     <div >收入</div>
+    <span :class="{'selected':selected}"></span>
   </div>
 </template>
 
 <script lang="ts" setup>
+import {onMounted, ref} from "vue";
+const selected=ref<boolean>(false)
+const root=ref<HTMLDivElement>("")
+onMounted(()=>{
+  console.log(root.value.childNodes)
+})
 const emit = defineEmits(["update:type"])
 const toggle = (e: any) => {
   if (e.target.innerText === "收入") {
+    selected.value=true
     emit("update:type", "+")
   } else if (e.target.innerText === "支出") {
+    selected.value=false
     emit("update:type", "-")
   }
 }
@@ -21,13 +30,26 @@ const toggle = (e: any) => {
 .type {
   display: flex;
   justify-content: center;
-
+  position: relative;
+  >span{
+    transition: all 250ms;
+    width: 4em;
+    height: 2px;
+    background-color: #333;
+    position: absolute;
+    bottom: 0;
+    left: calc(25% - 2em);
+  }
   > div {
     flex-grow: 1;
     text-align: center;
     font-size: 24px;
     padding: 16px 0;
     position: relative;
+  }
+  >span.selected{
+    transition: all 250ms;
+    left: calc(75% - 2em);
   }
 }
 
